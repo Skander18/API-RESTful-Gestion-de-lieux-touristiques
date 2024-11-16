056e69239d4d80ac68e401515fc9d1a72e1e2ad2
Skander18 <mokni.skander25@gmail.com>
2024-11-16 14:41:02 +0100

    create models file

--- management_tourism/locations/models.py
+++ management_tourism/locations/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Lieu(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.PointField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Lieu : {self.name}"
+
+    class Meta:
+        verbose_name = "Lieu"
+        verbose_name_plural = "Lieux"    
